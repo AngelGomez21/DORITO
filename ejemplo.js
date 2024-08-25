1ab3ca0feb84fdba00121a53cd9e8bfd5f07c788
AngelGomez21 <aguilargomez21@gmail.com>
2024-08-24 21:28:20 -0600

    si

---
+++ ejemplo.js
@@ -0,0 +1,9 @@
+let A=[[3,4,5,6],[7,8,9]];
+
+const datosArreglo=()=>{
+    A.map((datos,index)=>{
+        datos.map((valores,i)=>{
+            console.log(`${i}=${valores}`);
+        })
+    });
+}
